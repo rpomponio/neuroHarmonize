ab98575574f5a4b3456f3ee927f1342a47ded1f8
rpomponio <raypomponio@gmail.com>
2019-09-12 13:41:57 -0400

    changed name to neuroHarmonize

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='neuroharmonize',
+setup(name='neuroHarmonize',
       version='0.1',
       description='Harmonization tools for multi-center neuroimaging studies.',
       long_description=readme(),
-      url='https://github.com/rpomponio/neuroharmonize',
+      url='https://github.com/rpomponio/neuroHarmonize',
       author='Ray Pomponio',
       author_email='raymond.pomponio@pennmedicine.upenn.edu',
       license='MIT',
-      packages=['neuroharmonize'],
+      packages=['neuroHarmonize'],
       install_requires=['neuroCombat', 'numpy', 'pandas', 'nibabel'],
       dependency_links=['http://github.com/ncullen93/neuroCombat'],
       zip_safe=False)
